9d5ad1ddf5935fd2057149c0ead38630032f1adb
sdiop8171-dev <sdiop8171@bronxinternationalhs.com>
2026-07-15 12:10:24 -0400

    Mark old misnamed files as DEPRECATED (styles.js, scripts.js)

--- scripts.js
+++ scripts.js
@@ -1,13 +1,2 @@
-const facts = [
-    "The word 'Quran' means 'The Recitation.'",
-    "Muslims believe the Quran was revealed over about 23 years.",
-    "The Quran is divided into 114 chapters called surahs.",
-    "It was first revealed in the Cave of Hira near Makkah.",
-    "Millions of Muslims around the world have memorized the entire Quran.",
-    "The Quran is written in Arabic."
-];
-
-function showFact() {
-    let random = Math.floor(Math.random() * facts.length);
-    document.getElementById("fact").innerHTML = facts[random];
-}
+// DEPRECATED: This file was mistakenly named scripts.js. The correct script file is script.js
+// and index.html references script.js. You can safely delete this file.
